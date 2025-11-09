fb2c81f8d93e6448073711fe3c5765b995ecfa05
sagar patil <sagarmp7731@gmail.com>
2025-11-09 22:47:07 +0530

    pendrive code

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ const os = require('os');
 
 const key = crypto.scryptSync('your-secret-password', 'some-salt', 32);
 /* live testing*/
-//const decryptedPath = path.join(app.getPath('temp'), 'decrypted-scorm');
-// let encryptedPath = null;
-let scormContentPath="scormContent"
+const decryptedPath = path.join(app.getPath('temp'), 'decrypted-scorm');
+let encryptedPath = null;
+let scormContentPath="scormcontent"
 
 let USBPath=null;
 
@@ -20,8 +20,8 @@ let USBPath=null;
 let htmlfilePath="htmlPage/launchpage.html"
 
 /*local  testing*/
-const decryptedPath = path.join(__dirname, 'decryptedFolder');
-let encryptedPath = path.join(__dirname, scormContentPath);
+// const decryptedPath = path.join(__dirname, 'decryptedFolder');
+// let encryptedPath = path.join(__dirname, scormContentPath);
 
 
 
@@ -44,16 +44,6 @@ function getAllowedSerials({ serialNumber }) {
       hostname: 'dhioo.venturaelearning.com',
       path: '/api/organisation/serialnumber',
     });
-
-
-    // const request = net.request({
-    //   method: 'POST',
-    //   protocol: 'http:',
-    //   hostname: 'localhost',     // ✅ Only the hostname here
-    //   port: 3300,                // ✅ Port must be passed separately
-    //   path: '/api/organisation/serialnumber',
-    // });
-
 
     // Optional: Add request body
 
@@ -202,7 +192,6 @@ function createWindow() {
     const courseEncryptedDir = path.join(encryptedPath, courseName);
     const courseDecryptedDir = path.join(decryptedPath, courseName);
 
-    console.log("call", courseName);
 
     try {
       // Check if encrypted directory exists
@@ -245,9 +234,9 @@ function createWindow() {
   /*load html file present in pendrive like lauchpage.html inside htmlpage */
   ipcMain.on('open-local-html', (event) => {
     /*local testing */
-    if(!USBPath){
-      USBPath=__dirname
-    }
+    // if(!USBPath){
+    //   USBPath=__dirname
+    // }
 
    let filePath= path.join(USBPath, htmlfilePath)
     
@@ -265,7 +254,7 @@ function createWindow() {
         preload: path.join(__dirname, 'preload.js')
       }
     });
-    
+
     htmlWin.removeMenu();
     htmlWin.webContents.on('context-menu', e => e.preventDefault());
     htmlWin.loadURL(`file://${filePath}`);
@@ -353,52 +342,52 @@ app.whenReady().then(async () => {
 
 
   // Step 2: get USB PATH
-  // const usbPath = findUSBDrivePath();
-  // USBPath=usbPath;
-  // if (!usbPath) {
-  //   console.log("No USB drive detected.");
-  //           /*Error message*/
+  const usbPath = findUSBDrivePath();
+  if (!usbPath) {
+    console.log("No USB drive detected.");
+            /*Error message*/
 
-  //   dialog.showErrorBox("Fail to varify ", "No ROOT USB drive path detected. Please contact your regional Rieter sales team with the USB serial number (found on the USB cover)");
-  //   app.quit();
+    dialog.showErrorBox("Fail to varify ", "No ROOT USB drive path detected. Please contact your regional Rieter sales team with the USB serial number (found on the USB cover)");
+    app.quit();
 
-  // } else {
-  //   /*we updated global variable  encryptedPath*/
-  //   encryptedPath = path.join(usbPath, 'R');
-  //   /*check is path is exit or not*/
-  //   if (!fs.existsSync(encryptedPath)) {
-  //             /*Error message*/
-  //     dialog.showErrorBox("Fail to varify content path", "Please contact your regional Rieter sales team with the USB serial number (found on the USB cover).");
-  //     app.quit();
+  } else {
+    USBPath=usbPath;
+    /*we updated global variable  encryptedPath*/
+    encryptedPath = path.join(usbPath, scormContentPath);
+    /*check is path is exit or not*/
+    if (!fs.existsSync(encryptedPath)) {
+              /*Error message*/
+      dialog.showErrorBox("Fail to varify content path", "Please contact your regional Rieter sales team with the USB serial number (found on the USB cover).");
+      app.quit();
 
-  //   }
-  // }
+    }
+  }
 
 
 
 
   // Step 3: send serial number to server to check is it is valide and  not expire pendrive by  `net
-  // let allowedSerials = {};
-  // try {
-  //   allowedSerials = await getAllowedSerials({ serialNumber: result });
-  //   console.log("allowedSerials", allowedSerials)
-  // } catch (err) {
-  //   // Check for known network-related error codes
+  let allowedSerials = {};
+  try {
+    allowedSerials = await getAllowedSerials({ serialNumber: result });
+    console.log("allowedSerials", allowedSerials)
+  } catch (err) {
+    // Check for known network-related error codes
 
-  //   dialog.showErrorBox(
-  //     err?.title || "Error",
-  //     err?.message || "An unexpected error occurred during license verification."
-  //   );
+    dialog.showErrorBox(
+      err?.title || "Error",
+      err?.message || "An unexpected error occurred during license verification."
+    );
 
-  //   app.quit();
-  //   return;
-  // }
-  // // Step 4: on the bases of allowedSerials check whether user can allow to view content or quit app
-  // if (!allowedSerials?.valid) {
-  //   dialog.showErrorBox(allowedSerials?.title??"Fail to verify", allowedSerials?.message ?? "");
-  //   app.quit();
-  //   return;
-  // }
+    app.quit();
+    return;
+  }
+  // Step 4: on the bases of allowedSerials check whether user can allow to view content or quit app
+  if (!allowedSerials?.valid) {
+    dialog.showErrorBox(allowedSerials?.title??"Fail to verify", allowedSerials?.message ?? "");
+    app.quit();
+    return;
+  }
 
   // Step 5: Launch app
   createWindow();
